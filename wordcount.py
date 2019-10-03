487ea7c42c2b8f495022b388fdddedf3d2b992b6
arjunbansal090 <arjunbansal1230@gmail.com>
2019-10-03 23:25:04 +0530

    Task - To count no. of words in a sentence
    Signed-off-by: arjunbansal090 <arjunbansal1230@gmail.com>

---
+++ wordcount.py
@@ -0,0 +1,11 @@
+# To count words in string
+# Using split()
+# Initializing string
+test_string = "My name is Arjun Bansal"
+# Printing original string
+print ("The original string is : " +  test_string)
+# Using split()
+# To count words in string
+result = len(test_string.split())
+# Printing result
+print ("The number of words in string are : " +  str(result))
